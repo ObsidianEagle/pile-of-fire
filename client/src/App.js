cdaf24a5b007d65cb9e7188607e1e8a1690080f4
Alex King <alex.king@and.digital>
2020-10-27 00:21:44 +0000

    Adopt redux-style message structure for communication

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,21 @@ import './App.css';
 const App = () => {
   const ws = useMemo(() => new WebSocket('ws://localhost:8080'), []);
 
+  // Open socket after entering data, client sends init
   useEffect(() => {
-    ws.onopen = () => ws.send('sup bro');
-    ws.onmessage = (e) => console.log(e.data);
+    ws.onopen = () => ws.send(JSON.stringify({ type: 'PLAYER_INIT', payload: { name: 'hello' } }));
+    ws.onmessage = (e) => {
+      const msg = JSON.parse(e.data);
+      switch (msg.type) {
+        case 'PLAYER_INIT_ACK':
+          console.log(msg.payload);
+          ws.send(JSON.stringify({ type: 'MAKE_MOVE', payload: { suit: 'C', value: '4' } }));
+          break;
+        default:
+          console.log(msg.payload);
+          break;
+      }
+    }
   }, [ws]);
 
   return (
--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,50 @@
 import WebSocket from 'ws';
 
 const wss = new WebSocket.Server({ port: 8080 });
+const clients = [];
+let counter = 0;
 
 wss.on('connection', ws => {
-    ws.on('message', msg => {
-        console.log(`received: ${msg}`);
-        ws.send(`received: ${msg}`);
+    ws.id = counter++;
+    clients.push(ws);
+
+    ws.on('open', () => {
+        console.log(`client ${ws.id}: connection opened`);
     });
+
+    ws.on('message', reqString => {
+        const req = JSON.parse(reqString);
+        console.log(req);
+        switch (req.type) {
+            case 'PLAYER_INIT':
+                ws.name = req.payload.name;
+                const res = {
+                    type: 'PLAYER_INIT_ACK',
+                    payload: { id: ws.id }
+                };
+                ws.send(JSON.stringify(res));
+                console.log(`client ${ws.id}: player initiated with name ${ws.name}`);
+                break;
+            case 'MAKE_MOVE':
+                const { suit, value } = req.payload;
+                console.log(`client ${ws.id}: received move ${suit}${value}`);
+            default:
+                break;
+        }
+    });
+
+    ws.on('close', () => {
+        clients.splice(clients.findIndex(client => client.id === ws.id), 1);
+        console.log(`client ${ws.id}: connection closed`);
+    });
+
+    ws.on('error', err => {
+        console.log(`client ${ws.id}: error: ${err.message}`);
+    });
+});
+
+wss.on('error', () => {
+    const msgObject = { error: "Server encountered an error" };
+    const msgString = JSON.stringify(msgObject);
+    clients.forEach(client => client.send(msgString));
 });
