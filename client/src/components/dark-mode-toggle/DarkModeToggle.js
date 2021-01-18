e0a8a5ecda569088caa9debd5df374eb4a9b0d13
Alex King <alex.king@and.digital>
2021-01-18 19:55:30 +0000

    Add dark mode styling to landing page

--- client/src/components/dark-mode-toggle/DarkModeToggle.js
+++ client/src/components/dark-mode-toggle/DarkModeToggle.js
@@ -1,7 +1,8 @@
 import { Checkbox } from 'semantic-ui-react';
+import './DarkModeToggle.scss';
 
 const DarkModeToggle = ({ isDark, toggleDarkMode }) => (
-  <Checkbox toggle checked={isDark} onChange={toggleDarkMode} />
+  <Checkbox toggle checked={isDark} onChange={toggleDarkMode} className="dark-mode-toggle" />
 );
 
 export default DarkModeToggle;
