db449e3063d163c6cdd443d9a0fe229e5ed1e120
Alex King <alex.king@and.digital>
2021-02-21 14:21:16 +0000

    Add button to copy room link to clipboard

--- client/src/components/room-code-display/RoomCodeDisplay.js
+++ client/src/components/room-code-display/RoomCodeDisplay.js
@@ -1,8 +1,17 @@
+import { Icon } from 'semantic-ui-react';
 import './RoomCodeDisplay.scss';
 
 const RoomCodeDisplay = ({ roomCode }) => (
   <div className="room-code-display">
-    <h3>Room Code: {roomCode}</h3>
+    <h3>
+      Room Code: {roomCode}{' '}
+      <button
+        className="room-code-button"
+        onClick={() => navigator.clipboard.writeText(`${window.location.href}?room=${roomCode}`)}
+      >
+        <Icon name="copy" />
+      </button>
+    </h3>
   </div>
 );
 
