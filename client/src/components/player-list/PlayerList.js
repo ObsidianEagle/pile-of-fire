4640df30746dc975229e1d5803ad2e812c9320af
Alex King <alex.king@and.digital>
2021-02-25 09:56:52 +0000

    Change player list

--- client/src/components/player-list/PlayerList.js
+++ client/src/components/player-list/PlayerList.js
@@ -8,11 +8,10 @@ const PlayerList = ({ players, thisPlayerName, nextPlayerName }) => (
     <Card.Content>
       <ol>
         {players.map((player) => (
-          <li key={player.name}>{player.name}</li>
+          <li key={player.name}>{player.name === thisPlayerName ? <b>{player.name}</b> : player.name}</li>
         ))}
       </ol>
       <p style={{ textAlign: 'center' }}>
-        You are <b>{thisPlayerName}</b><br />
         Next up is {nextPlayerName === thisPlayerName ? <b>{nextPlayerName}</b> : nextPlayerName}
       </p>
     </Card.Content>
