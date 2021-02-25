d25221eee8900806ba0f42ea2c4890113a325056
Alex King <alex.king@and.digital>
2021-02-25 10:13:32 +0000

    Replace card description with markdown

--- client/src/components/turn-display/TurnDisplay.js
+++ client/src/components/turn-display/TurnDisplay.js
@@ -1,8 +1,10 @@
 import { useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
 import { Container, Grid } from 'semantic-ui-react';
 import { rules } from '../../constants/rules';
 import { useAudio } from '../../hooks';
 import PlayingCard from '../playing-card/PlayingCard';
+import "./TurnDisplay.scss";
 
 const TurnDisplay = ({ playerName, card, mobile }) => {
   const [, setAudioPlaying] = useAudio();
@@ -21,10 +23,10 @@ const TurnDisplay = ({ playerName, card, mobile }) => {
 
   const desktopView = (
     <Container textAlign="center">
-      <h3>{playerName || "The last player"} drew...</h3>
+      <h3>{playerName || 'The last player'} drew...</h3>
       <PlayingCard card={card} large />
       <h1>{name}</h1>
-      <h3>{description}</h3>
+      <ReactMarkdown className="turn-description">{description}</ReactMarkdown>
     </Container>
   );
 
@@ -34,9 +36,9 @@ const TurnDisplay = ({ playerName, card, mobile }) => {
         <PlayingCard card={card} large />
       </Grid.Column>
       <Grid.Column width={9}>
-        <h3>{playerName || "The last player"} drew...</h3>
+        <h3>{playerName || 'The last player'} drew...</h3>
         <h1>{name}</h1>
-        <h3>{description}</h3>
+        <ReactMarkdown className="turn-description">{description}</ReactMarkdown>
       </Grid.Column>
     </Grid>
   );
--- client/src/constants/rules.js
+++ client/src/constants/rules.js
@@ -1,53 +1,59 @@
 export const rules = {
-  'A': {
+  A: {
     name: 'Captain',
-    description: 'Make any player do anything at any time (as long as it doesn\'t break the core rules).'
+    description: "Make any player do anything at any time (as long as it doesn't break the core rules)."
   },
-  '2': {
+  2: {
     name: 'You',
     description: 'Choose a player to drink.'
   },
-  '3': {
+  3: {
     name: 'Me',
     description: 'Drink.'
   },
-  '4': {
+  4: {
     name: 'Never Have I Ever',
-    description: 'Put up 3 fingers, go around the players and play Never Have I Ever. Ends when a player has no fingers up.'
+    description:
+      'Put up 3 fingers, go around the players and play Never Have I Ever. Ends when a player has no fingers up.'
   },
-  '5': {
+  5: {
     name: 'Thumb Master',
     description: 'At any time, cover your camera with your thumb. Last player to follow drinks.'
   },
-  '6': {
+  6: {
     name: 'Everyone',
     description: 'Everyone drinks!'
   },
-  '7': {
+  7: {
     name: 'Heaven',
     description: 'Put your hand up. Last player to follow drinks.'
   },
-  '8': {
+  8: {
     name: 'Mate',
     description: 'Pick a mate. Every time you drink, they drink, and vice versa.'
   },
-  '9': {
+  9: {
     name: 'Rhyme Time, Categories, or Gecko',
-    description: 'Choose gecko, you know you want to.'
+    description: `**Rhyme Time**: Say a word. Go around and each say a word which rhymes. First player to take too long or repeats drinks.
+
+**Categories**: Declare a category and give something in that category. Go around and each give something in that category. First player to take too long or repeats drinks.
+
+**Gecko**: Last player to have at least 3 limbs on a wall drinks. Any players who refuse to try drink double.`
   },
-  '10': {
+  10: {
     name: 'Waterfall',
-    description: 'Everyone start drinking. Don\'t stop until the player before you in the player list stops.'
+    description: "Everyone start drinking. Don't stop until the player before you in the player list stops.",
+    showPlayerOrder: true
   },
-  'J': {
+  J: {
     name: 'Make A Rule',
     description: 'Make a rule that everyone must follow until the end of the game.'
   },
-  'Q': {
+  Q: {
     name: 'Question Master',
     description: 'If anyone answers a question you ask, they must drink.'
   },
-  'K': {
+  K: {
     name: '???',
     description: '???'
   }
