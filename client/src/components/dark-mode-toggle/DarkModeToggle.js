0e28b699529bf7728e298f7a7b72d16101e925fe
Alex King <alex.king@and.digital>
2021-03-07 16:10:11 +0000

    Add sun/moon icons to dark mode switch

--- client/src/components/dark-mode-toggle/DarkModeToggle.js
+++ client/src/components/dark-mode-toggle/DarkModeToggle.js
@@ -1,8 +1,29 @@
-import { Checkbox } from 'semantic-ui-react';
+import Switch from 'react-switch';
 import './DarkModeToggle.scss';
+import { Icon } from 'semantic-ui-react';
+
+const darkIcon = (
+  <div className="icon-wrapper">
+    <Icon name="moon" className="icon" />
+  </div>
+);
+
+const lightIcon = (
+  <div className="icon-wrapper">
+    <Icon name="sun" className="icon" />
+  </div>
+);
 
 const DarkModeToggle = ({ isDark, toggleDarkMode }) => (
-  <Checkbox toggle checked={isDark} onChange={toggleDarkMode} className="dark-mode-toggle" />
+  <Switch
+    checked={isDark}
+    onChange={toggleDarkMode}
+    className="dark-mode-toggle"
+    onColor="#707070"
+    offColor="#A0A0A0"
+    uncheckedIcon={lightIcon}
+    checkedIcon={darkIcon}
+  />
 );
 
 export default DarkModeToggle;
